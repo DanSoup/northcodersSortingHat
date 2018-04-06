58ef2d9e1e989f405ee076479bfdad61049bedbd
DanSoup <dansoup@hotmail.co.uk>
2018-04-06 15:06:19 +0100

    heroku 2

--- listen.js
+++ listen.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const port = process.env.PORT || 9090
 
-app.listen(9090, (err) => {
-    console.log('listening on port 9090')
+app.listen(port, (err) => {
+    console.log(`listening on port ${port}`)
 });
