4869a0a8d7bc4cd51d49d8c3eb4338f8da100d57
DanSoup <dansoup@hotmail.co.uk>
2018-04-06 15:50:18 +0100

    heroku 3

--- controllers/twitter.js
+++ controllers/twitter.js
@@ -3,10 +3,11 @@ let consumer_key = process.env.consumer_key;
 let consumer_secret = process.env.consumer_secret
 let access_token = process.env.access_token
 let access_token_secret = process.env.access_token_secret;
+const keysTokens = {};
 if (consumer_key === undefined) {
   keysTokens = require('../config/twitterCredentials.js');
 } else {
-  const keysTokens = {consumer_key, consumer_secret, access_token, access_token_secret}
+  keysTokens = {consumer_key, consumer_secret, access_token, access_token_secret}
 }
 const getPersonality = require('./personality');
 
